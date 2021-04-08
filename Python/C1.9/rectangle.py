6c20ef977c5b6bbad906322751d6e823bb180116
Denis <tetragrom@mail.ru>
2021-04-08 22:37:48 +0400

    Задание С1.9.1

--- Python/C1.9/rectangle.py
+++ Python/C1.9/rectangle.py
@@ -1,3 +1,5 @@
+import math
+
 class Rectangle:
     def __init__(self, width, heigth):
         self.width = width
@@ -11,3 +13,18 @@ class Rectangle:
 
     def get_area(self):
         return self.width * self.height
+
+
+class Square:
+    def __init__(self, width):
+	    self.width = width
+
+    def get_area_square(self):
+        return self.width ** 2
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_area_circle(self):
+        return round(self.radius ** 2 * math.pi, 3)
--- Python/C1.9/testRectangle.py
+++ Python/C1.9/testRectangle.py
@@ -1,18 +1,45 @@
-from rectangle import Rectangle
+from rectangle import Rectangle, Square, Circle
 
-r1 = Rectangle(10, 5)
+# r1 = Rectangle(10, 5)
+#
+# print("r1.width =", r1.width)
+# print("r1.height =", r1.height)
+# print("r1.get_width =", r1.get_width())
+# print("r1.get_height =", r1.get_height())
+# print("r1.get_area =", r1.get_area())
+# print('-----')
+# r2 = Rectangle(6, 8)
+#
+# print("r2.width =", r2.width)
+# print("r2.height =", r2.height)
+# print("r2.get_width =", r2.get_width())
+# print("r2.get_height =", r2.get_height())
+# print("r2.get_area =", r2.get_area())
+# print('-----')
 
-print("r1.width =", r1.width)
-print("r1.height =", r1.height)
-print("r1.get_width =", r1.get_width())
-print("r1.get_height =", r1.get_height())
-print("r1.get_area =", r1.get_area())
-print('-----')
-r2 = Rectangle(6, 8)
+rect_1 = Rectangle(3,4)
+rect_2 = Rectangle(12,5)
+#вывод площади наших двух прямоугольников
+print(rect_1.get_area())
+print(rect_2.get_area())
 
-print("r1.width =", r2.width)
-print("r1.height =", r2.height)
-print("r1.get_width =", r2.get_width())
-print("r1.get_height =", r2.get_height())
-print("r1.get_area =", r2.get_area())
+square_1 = Square(5)
+square_2 = Square(10)
 
+print(square_1.get_area_square(), ',',
+      square_2.get_area_square())
+
+
+figures = [rect_1, rect_2, square_1, square_2]
+
+for figure in figures:
+    if isinstance(figure, Square):
+        print(figure.get_area_square())
+    else:
+        print(figure.get_area())
+
+circle_1 = Circle(4)
+circle_2 = Circle(8)
+
+print('circle_1 = ', circle_1.get_area_circle())
+print('circle_2 = ', circle_2.get_area_circle())
